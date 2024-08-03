8f5d36c9f8507c7008e97ca104f7110721081319
ozzo_254 <veetruth99@gmail.com>
2024-08-03 05:45:01 -0400

    .

--- client/src/Pages/admin/MessagesManagement.jsx
+++ client/src/Pages/admin/MessagesManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
 import Table from '../../components/shared/Table';
 import { dashboardData } from '../../constants/sampleData';
